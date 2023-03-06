3d9c4b05f6ba85a8a4d8a9ac2f6fdc10f8d3fa9e
ajrox <ajrocks0509@gmail.com>
2023-03-06 17:42:56 +0100

    able to print event sequence for Zest Fuzzer

--- data-extract/src/main/kotlin/runner.kt
+++ data-extract/src/main/kotlin/runner.kt
@@ -1,9 +1,15 @@
 import edu.berkeley.cs.jqf.fuzz.ei.ZestDriver
 import edu.berkeley.cs.jqf.fuzz.ei.ZestGuidance
+import edu.berkeley.cs.jqf.fuzz.guidance.Result
 import edu.berkeley.cs.jqf.fuzz.junit.GuidedFuzzing
+import edu.berkeley.cs.jqf.fuzz.util.ICoverage
+import edu.berkeley.cs.jqf.instrument.tracing.events.BranchEvent
+import edu.berkeley.cs.jqf.instrument.tracing.events.CallEvent
+import edu.berkeley.cs.jqf.instrument.tracing.events.ReturnEvent
 import edu.berkeley.cs.jqf.instrument.tracing.events.TraceEvent
 import java.io.File
-import java.lang.Boolean
+import java.io.InputStream
+import java.security.MessageDigest
 import java.time.Duration
 import kotlin.Array
 import kotlin.String
@@ -12,14 +18,50 @@ import kotlin.system.exitProcess
 
 class MeasureZest(testName: String?, duration: Duration?, outputDirectory: File?) :
     ZestGuidance(testName, duration, outputDirectory) {
+    // call event -> 0, return event -> 1, branch event -> 2, total events -> 3
+    var events = mutableListOf<TraceEvent?>()
+
+    override fun displayStats(force: kotlin.Boolean) {
+//        super.displayStats(force)
+    }
+
+    override fun getTotalCoverage(): ICoverage<*> {
+        return super.getTotalCoverage()
+    }
+
+    override fun getInput(): InputStream {
+        return super.getInput()
+    }
 
     override fun handleEvent(e: TraceEvent?) {
+        events.add(e)
         super.handleEvent(e)
     }
 
-//    override fun checkSavingCriteriaSatisfied(result: Result?): List<String> {
-//        return listOf("dumpall")
-//    }
+    private fun hash(str:String): String {
+            val bytes = str.toByteArray()
+            val md = MessageDigest.getInstance("SHA-256")
+            val digest = md.digest(bytes)
+            return digest.fold("") { s, it -> s + "%02x".format(it) }.substring(0,6)
+
+    }
+
+    override fun handleResult(result: Result?, error: Throwable?) {
+        println("total events for input ${this.numTrials}: ${events.size}")
+        events.toList().filterIsInstance<BranchEvent>().take(40).forEach { print(hash(it.iid.toString())+" ") }
+        println()
+        events = mutableListOf()
+        super.handleResult(result, error)
+        val maxAllowedInputs = 11
+        if (this.numTrials > maxAllowedInputs){
+            println("Finished $maxAllowedInputs trials")
+            exitProcess(0)
+        }
+    }
+
+    override fun checkSavingCriteriaSatisfied(result: Result?): List<String> {
+        return listOf("dumpall")
+    }
 }
 
 
@@ -43,17 +85,6 @@ fun main(args: Array<String>) {
 
         // Run the Junit test
         val res = GuidedFuzzing.run(testClassName, testMethodName, guidance, System.out)
-        if (Boolean.getBoolean("jqf.logCoverage")) {
-            println(
-                String.format(
-                    "Covered %d edges.",
-                    guidance.totalCoverage.nonZeroCount
-                )
-            )
-        }
-        if (Boolean.getBoolean("jqf.ei.EXIT_ON_CRASH") && !res.wasSuccessful()) {
-            exitProcess(3)
-        }
     } catch (e: Exception) {
         e.printStackTrace()
         exitProcess(2)
