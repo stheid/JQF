ab0e88ccf3256915c4e5b8e15fadabb7c9fbf49b
Stefan Heid <stefan.heid@upb.de>
2023-03-08 16:08:19 +0100

    added timing and corrected data output to export the whole int instead of byte

--- data-extract/src/main/kotlin/runner.kt
+++ data-extract/src/main/kotlin/runner.kt
@@ -5,7 +5,6 @@ import edu.berkeley.cs.jqf.fuzz.junit.GuidedFuzzing
 import edu.berkeley.cs.jqf.fuzz.util.ICoverage
 import edu.berkeley.cs.jqf.instrument.tracing.events.BranchEvent
 import edu.berkeley.cs.jqf.instrument.tracing.events.TraceEvent
-import org.eclipse.collections.api.iterator.IntIterator
 import org.eclipse.collections.impl.set.mutable.primitive.IntHashSet
 import java.io.BufferedOutputStream
 import java.io.DataOutputStream
@@ -13,16 +12,17 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
-import java.security.MessageDigest
 import java.time.Duration
+import java.util.*
 import kotlin.system.exitProcess
 
 
 class MeasureZest(testName: String?, duration: Duration?, outputDirectory: File?) :
     ZestGuidance(testName, duration, outputDirectory) {
     // call event -> 0, return event -> 1, branch event -> 2, total events -> 3
-    var events = mutableListOf<Byte>()
+    var events = mutableListOf<Int>()
     val eventsFile = File("fuzz-results/events.bin").apply { bufferedWriter().write("") }
+
     override fun displayStats(force: kotlin.Boolean) {
 //        super.displayStats(force)
     }
@@ -36,8 +36,7 @@ class MeasureZest(testName: String?, duration: Duration?, outputDirectory: File?
     }
 
     override fun handleEvent(e: TraceEvent?) {
-        if(e is BranchEvent)
-            events.add(e.iid.toByte())
+        if (e is BranchEvent) events.add(e.iid)
         super.handleEvent(e)
     }
 
@@ -45,22 +44,25 @@ class MeasureZest(testName: String?, duration: Duration?, outputDirectory: File?
 //        println("total events for input ${this.numTrials}: ${events.size}")
 //        events.toList().filterIsInstance<BranchEvent>().take(40).forEach { print(hash(it.iid.toString())+" ") }
 //        println()
-        BufferedOutputStream(FileOutputStream(eventsFile, true))
-            .use { it.write(events.toList().toByteArray()) }
-
+        val eventList = events.toList()
+        DataOutputStream(BufferedOutputStream(FileOutputStream(eventsFile, true))).also { dst ->
+            dst.writeInt(eventList.size)
+            eventList.forEach(dst::writeInt)
+        }
 
         events = mutableListOf()
         super.handleResult(result, error)
         val maxAllowedInputs = 1000
 
-        if (numTrials >= maxAllowedInputs){
+        if (numTrials >= maxAllowedInputs) {
             println("Finished $numTrials trials")
+            println("Time consumed ${"%.2f".format((Date().time - startTime.time) / 1000.0)}s")
             exitProcess(0)
         }
     }
 
     override fun checkSavingCriteriaSatisfied(result: Result?): List<String> {
-        return super.checkSavingCriteriaSatisfied(result).apply { add("dumpall")}
+        return super.checkSavingCriteriaSatisfied(result).apply { add("dumpall") }
     }
 
     @Throws(IOException::class)
