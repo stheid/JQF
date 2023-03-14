130c057f1f4e68beb224fb3995aff655536d8250
ajrox <ajrocks0509@gmail.com>
2023-03-14 13:09:02 +0100

    working version of AFL gradle build and classpath.

--- scripts/classpath_gradle.sh
+++ scripts/classpath_gradle.sh
@@ -6,15 +6,18 @@ SCRIPT_DIR=`pwd`
 popd > /dev/null
 
 # The root dir is one up
-ROOT_DIR=`dirname $SCRIPT_DIR`
+ROOT_DIR=`dirname "$SCRIPT_DIR"`
 
 # Create classpath
-cp="$ROOT_DIR/fuzz/build/classes/java/main:$ROOT_DIR/fuzz/build/classes/java/test"
+cp="$ROOT_DIR/fuzz/build/classes/java/main:$ROOT_DIR/fuzz/build/classes/java/test:$ROOT_DIR/examples/build/resources/test:$ROOT_DIR/examples/build/classes/java/test"
 
 for jar in $ROOT_DIR/fuzz/build/libs/*.jar; do
   cp="$cp:$jar"
 done
 for jar in $ROOT_DIR/data-extract/build/libs/*.jar; do
+  cp="$cp:$jar"
+done
+for jar in $ROOT_DIR/examples/build/libs/*.jar; do
   cp="$cp:$jar"
 done
 echo $cp
