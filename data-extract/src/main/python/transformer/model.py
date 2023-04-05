6ebf07a81414a9835322f175aaffa9f1026347a6
ajrox <ajrocks0509@gmail.com>
2023-04-05 13:17:58 +0200

    adding more debug to model.py

--- data-extract/src/main/python/transformer/model.py
+++ data-extract/src/main/python/transformer/model.py
@@ -61,8 +61,11 @@ class TransformerModel:
         return self.transformer is not None
 
     def train(self, data: Dataset, val_data: Dataset, epochs, pre_train=True):
+        logger.debug("preprocessing train data")
         train = self.preprocess_data_transformer(data, pretrain=pre_train)
+        logger.debug("preprocessing val data")
         val = self.preprocess_data_transformer(val_data)
+        logger.debug("fitting transformer")
         self.transformer.fit(train, epochs=epochs, validation_data=val)
 
     def predict(self, seq) -> bytes:
